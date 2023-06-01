90a4e918cb9bdacb97c229a54d25fa6e28341b64
sameh-Tawfiq <58302710+sameh-Tawfiq@users.noreply.github.com>
2023-06-01 18:35:43 +0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,10 @@
+
+variable "ami" {
+    description= "to define the ami of ec2 instance"
+    type= string 
+}
+
+variable "instance_type" {
+    description= "to define the instance_type of ec2 instance"
+    type= string 
+}
